037428389a6f26657e1e785aca14c8d36738c5f7
Chris Missal <chris.missal@gmail.com>
2019-09-19 20:16:37 -0500

    initial (nothing) commit

---
+++ Basher/Parser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Basher
+{
+    public class Parser
+    {
+        private readonly CancellationToken cancellationToken;
+
+        public Parser(CancellationToken cancellationToken)
+        {
+            this.cancellationToken = cancellationToken;
+        }
+
+        public Task ParseFromFileAsync(string fileName)
+        {
+            return Task.Delay(10000, cancellationToken);
+        }
+    }
+}
---
+++ Client/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Basher;
+
+namespace Client
+{
+    class Program
+    {
+        static readonly CancellationTokenSource cancellationTokenSource = new CancellationTokenSource(TimeSpan.FromMinutes(2));
+
+        static int Main(string[] args)
+        {
+            var fileName = args.Any() ? args[0] : @"C:\Program Files (x86)\Steam\steamapps\common\dota 2 beta\game\dota\replays\4984038549.dem";
+
+            var cancellationToken = cancellationTokenSource.Token;
+
+            var cancelTask = Task.Factory.StartNew(() =>
+            {
+                var key = Console.ReadKey();
+                if (key.Key == ConsoleKey.Escape)
+                {
+                    cancellationTokenSource.Cancel(true);
+                }
+            }, CancellationToken.None);
+
+            var shortFileName = fileName.Split('/').Reverse().First();
+            Console.WriteLine($"Beginning to parse {shortFileName}");
+
+            var parser = new Parser(cancellationToken);
+            var parseTask = parser.ParseFromFileAsync(fileName);
+
+            Task.WhenAny(cancelTask, parseTask).ConfigureAwait(false).GetAwaiter().GetResult();
+
+            var exitCode = cancelTask.IsCompletedSuccessfully
+                ? 100 - (int) parseTask.Status // error code, sure
+                : TaskStatus.RanToCompletion - parseTask.Status; // success = 0
+
+            Console.WriteLine($"Exited with {exitCode}");
+#if DEBUG
+            Console.ReadLine();
+#endif
+            return exitCode;
+        }
+    }
+}
