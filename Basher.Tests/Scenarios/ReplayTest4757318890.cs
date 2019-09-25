d7900e27323a3dee352be6a9e4f258bd3f5a6169
Chris Missal <chris.missal@gmail.com>
2019-09-25 05:16:11 -0500

    adds some new initial test expectations

--- Basher.Tests/Scenarios/ReplayTest4757318890.cs
+++ Basher.Tests/Scenarios/ReplayTest4757318890.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace Basher.Tests.Scenarios
 {
     public class ReplayTest4757318890 : ReplayTestBase
@@ -5,6 +7,12 @@ namespace Basher.Tests.Scenarios
         public ReplayTest4757318890() : base("4757318890")
         {
             this.ExpectedSummaryOffset = 114563495uL;
+            this.Victor = "Radiant";
+            this.RadiantKills = 41;
+            this.DireKills = 38;
+            this.Duration = new TimeSpan(0, 41, 35);
+            this.GameMode = "Captains Mode";
+            this.MatchDate = new DateTimeOffset(2019, 5, 16, 12, 4, 0, TimeSpan.Zero);
         }
     }
 }
